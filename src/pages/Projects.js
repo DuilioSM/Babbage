c861bc46c4ecb7deb48788f231e39643496eba60
Duilio <duilio@Duilios-MacBook-Air.local>
2022-08-16 00:12:03 -0500

    comente algunos componentes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,24 +1,24 @@
 import React from "react";
-import Budget from "../components/Budget";
-import Cisco from "../components/Cisco";
-import Croquis from "../components/Croquis";
+// import Budget from "../components/Budget";
+// import Cisco from "../components/Cisco";
+// import Croquis from "../components/Croquis";
 // import DownloadJava from "../components/DownloadJava";
-import Gantt from "../components/Gantt";
+// import Gantt from "../components/Gantt";
 import Manual from "../components/Manual";
 import Problem from "../components/Problem";
-import TableCisco from "../components/TableCisco";
+// import TableCisco from "../components/TableCisco";
 import WebApp from "../components/WebApp";
 
 function Projects() {
   return (
     <>
       <Problem />
-      <Croquis />
-      <Budget />
+      {/* <Croquis />
+      <Budget /> */}
       <WebApp />
-      <TableCisco />
+      {/* <TableCisco />
       <Cisco />
-      <Gantt />
+      <Gantt /> */}
       <Manual />
       {/* <DownloadJava /> */}
     </>
